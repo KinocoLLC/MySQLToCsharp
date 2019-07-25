b66eabee97cd8cd9d2c7dd79f1af15d605ed15e8
Ikiru Yoshizaki <3856350+guitarrapc@users.noreply.github.com>
2019-07-25 20:32:29 +0900

    chore: add document comment

--- MySQLToCsharp/Extensions/StringExtensions.cs
+++ MySQLToCsharp/Extensions/StringExtensions.cs
@@ -6,11 +6,29 @@ namespace MySQLToCsharp
 {
     public static class StringExtensions
     {
+        /// <summary>
+        /// remove () from string. (xxx) -> xxx
+        /// </summary>
+        /// <param name="text"></param>
+        /// <returns></returns>
         public static string RemoveParenthesis(this string text) => text?.Replace("(", "").Replace(")", "");
-
+        /// <summary>
+        /// remove ` from string. `xxx` -> xxx
+        /// </summary>
+        /// <param name="text"></param>
+        /// <returns></returns>
         public static string RemoveBackQuote(this string text) => text?.Replace("`", "");
-
+        /// <summary>
+        /// remove ' from string. 'xxx' -> xxx
+        /// </summary>
+        /// <param name="text"></param>
+        /// <returns></returns>
         public static string RemoveSingleQuote(this string text) => text?.Replace("'", "");
-
+        /// <summary>
+        /// remove DEFAULT'' from string. DEFAULT'xxx' -> xxx
+        /// </summary>
+        /// <param name="text"></param>
+        /// <returns></returns>
+        public static string RemoveDefaultString(this string text) => text?.Substring(0, text.Length - 1).Replace("DEFAULT'", "");
     }
 }
