7ed063fa33e03d9e9da8a24ccd5021494c3b8fb2
Ikiru Yoshizaki <3856350+guitarrapc@users.noreply.github.com>
2019-08-02 15:05:28 +0900

    feat: add TypeConverterResolver

--- src/MySQLToCSharp/Program.cs
+++ src/MySQLToCSharp/Program.cs
@@ -1,6 +1,7 @@
 ﻿using MicroBatchFramework;
 using Microsoft.Extensions.Logging;
 using MySQLToCsharp.Listeners;
+using MySQLToCsharp.TypeConverters;
 using System;
 using System.Linq;
 using System.Threading.Tasks;
@@ -16,38 +17,54 @@ namespace MySQLToCsharp
     }
     public class QueryToCSharp : BatchBase
     {
+        const string defaultConverter = nameof(StandardConverter);
+
         [Command(new[] { "--query", "-q" }, "read input sql and generate C# class.")]
         public void ParseString(
             [Option("-i", "mysql query to parse")]string input,
             [Option("-o", "directory path to output C# class file")]string output,
-            [Option("-n", "namespace to write")]string @namespace)
+            [Option("-n", "namespace to write")]string @namespace,
+            [Option("-c", "add bom or not")]string converter = defaultConverter,
+            [Option("--addbom", "add bom or not")]bool addbom = false)
         {
             var listener = new CreateTableStatementDetectListener();
             IParser parser = new Parser();
             parser.Parse(input, listener);
             var definition = listener.TableDefinition;
-            new Generator().Save(@namespace, definition, output);
+            var resolvedConverter = TypeConverterResolver.Resolve(converter);
+
+            Context.Logger.LogInformation($"Output Directory: {output}");
+            new Generator(addbom, resolvedConverter).Save(@namespace, definition, output);
         }
 
         [Command(new[] { "--file", "-f" }, "read specified .sql file and generate C# class.")]
         public void ParseFromFile(
             [Option("-i", "file path to parse mysql query")]string input,
             [Option("-o", "directory path to output C# class file")]string output,
-            [Option("-n", "namespace to write")]string @namespace)
+            [Option("-n", "namespace to write")]string @namespace,
+            [Option("-c", "add bom or not")]string converter = defaultConverter,
+            [Option("--addbom", "add bom or not")]bool addbom = false)
         {
             var definition = Parser.FromFile(input, false);
-            new Generator().Save(@namespace, definition, output);
+            var resolvedConverter = TypeConverterResolver.Resolve(converter);
+
+            Context.Logger.LogInformation($"Output Directory: {output}");
+            new Generator(addbom, resolvedConverter).Save(@namespace, definition, output);
         }
 
         [Command(new[] { "--dir", "-d" }, "list directory's *.sql file and generate C# class.")]
         public void ParseFromFolder(
             [Option("-i", "folder path to parse mysql query")]string input,
             [Option("-o", "directory path to output C# class file")]string output,
-            [Option("-n", "namespace to write")]string @namespace)
+            [Option("-n", "namespace to write")]string @namespace,
+            [Option("-c", "add bom or not")]string converter = defaultConverter,
+            [Option("--addbom", "add bom or not")]bool addbom = false)
         {
             var definitions = Parser.FromFolder(input, false).ToArray();
+            var resolvedConverter = TypeConverterResolver.Resolve(converter);
+            var generator = new Generator(addbom, resolvedConverter);
+
             Context.Logger.LogInformation($"Output Directory: {output}");
-            var generator = new Generator();
             foreach (var definition in definitions)
             {
                 generator.Save(@namespace, definition, output);
---
+++ src/MySQLToCSharp/TypeConverters/TypeConverterResolver.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MySQLToCsharp.TypeConverters
+{
+    public static class TypeConverterResolver
+    {
+        static readonly Dictionary<string, ITypeConverter> converterDict;
+
+        static TypeConverterResolver()
+        {
+            converterDict = new Dictionary<string, ITypeConverter>();
+            converterDict.Add(nameof(StandardConverter), new StandardConverter());
+        }
+        public static ITypeConverter Resolve(string key)
+        {
+            if (converterDict.TryGetValue(key, out var converter))
+            {
+                return converter;
+            }
+
+            throw new ArgumentOutOfRangeException($"Could not resolve converter '{key}'.");
+        }
+    }
+}
