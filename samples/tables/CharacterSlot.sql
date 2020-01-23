0e8ef0686c785d89500103caf52890f01cfb31f2
Ikiru Yoshizaki <3856350+guitarrapc@users.noreply.github.com>
2020-01-23 18:48:13 +0900

    chore: add sample output for DATETIME

--- samples/tables/CharacterSlot.sql
+++ samples/tables/CharacterSlot.sql
@@ -5,6 +5,7 @@ CREATE TABLE `CharacterSlots` (
   `WeaponId` INT NULL,
   `WeaponId2` INT NULL,
   `Exp` INT NOT NULL,
+  `Created` DATETIME NOT NULL,
   PRIMARY KEY (`Id`),
   UNIQUE INDEX `AK_CharacterSlot_Column` (`SlotIndex` ASC, `CharacterId` ASC)
 );
