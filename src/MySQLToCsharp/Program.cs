fe995491551dfbf1982145d82b60f321452702ad
Ikiru Yoshizaki <3856350+guitarrapc@users.noreply.github.com>
2020-01-24 01:41:16 +0900

    chore: update MicroBatchFramework to ConsoleAppFramework

--- src/MySQLToCsharp/Program.cs
+++ src/MySQLToCsharp/Program.cs
@@ -1,4 +1,5 @@
-﻿using MicroBatchFramework;
+using ConsoleAppFramework;
+using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using MySQLToCsharp.Listeners;
 using MySQLToCsharp.Parsers;
@@ -13,20 +14,23 @@ namespace MySQLToCsharp
     {
         static async Task Main(string[] args)
         {
-            await BatchHost.CreateDefaultBuilder().RunBatchEngineAsync<QueryToCSharp>(args);
+            await Host.CreateDefaultBuilder()
+                .ConfigureLogging(logging => logging.ReplaceToSimpleConsole())
+                .RunConsoleAppFrameworkAsync<QueryToCSharp>(args);
         }
     }
-    public class QueryToCSharp : BatchBase
+    public class QueryToCSharp : ConsoleAppBase
     {
         const string defaultConverter = nameof(StandardConverter);
+        readonly ILogger<QueryToCSharp> logger;
 
-        [Command(new[] { "--query", "-q" }, "read input sql and generate C# class.")]
+        public QueryToCSharp(ILogger<QueryToCSharp> logger) => (this.logger) = logger;
         public void ParseString(
-            [Option("-i", "mysql query to parse")]string input,
-            [Option("-o", "directory path to output C# class file")]string output,
+            [Option("-i", "input mysql ddl query to parse")]string input,
+            [Option("-o", "output directory path of generated C# class file")]string output,
             [Option("-n", "namespace to write")]string @namespace,
             [Option("-c", "converter name to use")]string converter = defaultConverter,
-            [Option("--addbom", "add bom or not")]bool addbom = false)
+            bool addbom = false,
         {
             var listener = new CreateTableStatementDetectListener();
             IParser parser = new Parser();
@@ -35,37 +39,38 @@ namespace MySQLToCsharp
             var resolvedConverter = TypeConverterResolver.Resolve(converter);
 
             Context.Logger.LogInformation($"Output Directory: {output}");
-            new Generator(addbom, resolvedConverter).Save(@namespace, definition, output);
+            logger.LogInformation($"Output Directory: {output}");
         }
 
         [Command(new[] { "--file", "-f" }, "read specified .sql file and generate C# class.")]
         public void ParseFromFile(
-            [Option("-i", "file path to parse mysql query")]string input,
-            [Option("-o", "directory path to output C# class file")]string output,
+            [Option("-i", "input file path to parse mysql ddl query")]string input,
+            [Option("-o", "output directory path of generated C# class file")]string output,
             [Option("-n", "namespace to write")]string @namespace,
             [Option("-c", "converter name to use")]string converter = defaultConverter,
-            [Option("--addbom", "add bom or not")]bool addbom = false)
+            bool addbom = false,
         {
             var definition = Parser.FromFile(input, false);
             var resolvedConverter = TypeConverterResolver.Resolve(converter);
 
             Context.Logger.LogInformation($"Output Directory: {output}");
-            new Generator(addbom, resolvedConverter).Save(@namespace, definition, output);
+            logger.LogInformation($"Output Directory: {output}");
         }
 
         [Command(new[] { "--dir", "-d" }, "list directory's *.sql file and generate C# class.")]
         public void ParseFromFolder(
-            [Option("-i", "folder path to parse mysql query")]string input,
-            [Option("-o", "directory path to output C# class file")]string output,
+            [Option("-i", "input folder path to parse mysql ddl query")]string input,
+            [Option("-o", "output directory path of generated C# class files")]string output,
             [Option("-n", "namespace to write")]string @namespace,
             [Option("-c", "converter name to use")]string converter = defaultConverter,
-            [Option("--addbom", "add bom or not")]bool addbom = false)
+            bool addbom = false,
         {
             var definitions = Parser.FromFolder(input, false).ToArray();
             var resolvedConverter = TypeConverterResolver.Resolve(converter);
             var generator = new Generator(addbom, resolvedConverter);
 
             Context.Logger.LogInformation($"Output Directory: {output}");
+            logger.LogInformation($"Output Directory: {output}");
             foreach (var definition in definitions)
             {
                 generator.Save(@namespace, definition, output);
