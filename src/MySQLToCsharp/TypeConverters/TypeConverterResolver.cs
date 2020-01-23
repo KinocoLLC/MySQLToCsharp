d316600d1a896cebd875ba1e2facf859a1944a49
Ikiru Yoshizaki <3856350+guitarrapc@users.noreply.github.com>
2020-01-23 18:35:54 +0900

    chore: search and load implemented classes

--- src/MySQLToCsharp/TypeConverters/TypeConverterResolver.cs
+++ src/MySQLToCsharp/TypeConverters/TypeConverterResolver.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Reflection;
-using System.Text;
 
 namespace MySQLToCsharp.TypeConverters
 {
@@ -15,9 +15,21 @@ namespace MySQLToCsharp.TypeConverters
             type = typeof(ITypeConverter);
             converterDict = new Dictionary<string, ITypeConverter>();
 
-            AddResolver(nameof(StandardConverter));
-            AddResolver(nameof(StandardBitAsBoolConverter));
-            AddResolver(nameof(StandardDateTimeAsOffsetConverter));
+            var implementations = GetImplementations(type.Assembly);
+            foreach (var implementation in implementations)
+            {
+                AddResolver(implementation);
+            }
+        }
+
+        public static Type[] GetImplementations(Assembly assembly)
+        {
+            var implementations = assembly.GetTypes()
+                .Where(x => type.IsAssignableFrom(x))
+                .Where(x => x.IsClass)
+                .Where(x => x.IsPublic)
+                .ToArray();
+            return implementations;
         }
 
         public static void AddResolver(string resolverType)
@@ -28,6 +40,15 @@ namespace MySQLToCsharp.TypeConverters
                 converterDict.Add(resolverType, instance);
             }
         }
+        public static void AddResolver(Type resolverType)
+        {
+            if (!converterDict.ContainsKey(resolverType.Name))
+            {
+                var instance = (ITypeConverter)Activator.CreateInstance(resolverType);
+                converterDict.Add(resolverType.Name, instance);
+            }
+        }
+
         public static ITypeConverter Resolve(string key)
         {
             if (converterDict.TryGetValue(key, out var converter))
